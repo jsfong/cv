bac7c088a41a9b334d52d7718d047ad0c2c675e0
jsfong <jiunnshian.fong@thalesgroup.com>
2020-09-02 19:46:12 +0800

    Update intro font size

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -6,12 +6,12 @@ function Introduction() {
     <div className={`container-fluid ${classes.title}`}>
       <div className="row">
         <div className="col-md-6">
-          <h4>Hello, I'm</h4>
+          <h4 className={classes.titleHello}>Hello, I'm</h4>
           <h1 className={classes.titleBig}>Js Fong</h1>
           <p>Software engineer based in Singapore</p>
         </div>
         <div className="col-md-6">
-          <img className={classes.titleImg} src="https://via.placeholder.com/150"></img>
+          <img className={classes.titleImg} src="https://via.placeholder.com/150" alt="Place holder"></img>
         </div>
       </div>
     </div>
