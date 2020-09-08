bfc4ceb07cb16c175ac5ab3399d742c9e890d752
jsfong <jiunnshian.fong@thalesgroup.com>
2020-09-08 18:31:57 +0800

    Update footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,15 +2,28 @@ import React from "react";
 import classes from './Footer.module.css';
 
 function Footer() {
+
+    const year = new Date().getFullYear();
+
+    console.log(year);
     return (
-        <footer className="white-section" id="footer">
+        <div className={classes.footerSection} id="footer">
             <div className="container-fluid">
-                <i className={`fab fa-linkedin ${classes.socialIcon}`}></i>
-                <i className={`fab fa-github ${classes.socialIcon}`}></i>
-                <i className={`fas fa-envelope ${classes.socialIcon}`}></i>
-                <p>© Copyright 2018 JsFong</p>
+                <a href="https://www.linkedin.com/in/js-fong-52a04534/" target="_blank">
+                    <i className={`fab fa-linkedin ${classes.socialIcon}`}></i>
+                </a>
+
+                <a href="https://github.com/jsfong" target="_blank">
+                    <i className={`fab fa-github ${classes.socialIcon}`}></i>
+                </a>
+
+                <a href="mailto:jsfong5105@gmail.com">
+                    <i className={`fas fa-envelope ${classes.socialIcon}`}></i>
+                </a>
+
+                <p className={classes.copyright}>© Copyright {year} JsFong</p>
             </div>
-        </footer>
+        </div>
     );
 
 
--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -9,10 +9,12 @@ function Introduction() {
           <h4 className={classes.titleHello}>Hello, I'm</h4>
           <h1 className={classes.titleBig}>Js Fong</h1>
           <p className={classes.description}>Software engineer based in Singapore</p>
-          <button type="button" className={`btn btn-dark btn-lg ${classes.downloadButton}`} href="#" >
+          <a type="button" className={`btn btn-dark btn-lg ${classes.downloadButton}`}
+            href="https://docs.google.com/document/d/1eBOD8U8qu6kdiChiqK8sOngw5JT3D5LBFuMV5DbEXEM/edit?usp=sharing"
+            target="_blank" >
             <i className="far fa-file-pdf"></i>
             &nbsp; Download CV
-          </button>
+          </a>
           <a className={`btn btn-dark btn-lg ${classes.downloadButton}`} href="mailto:jsfong5105@gmail.com"><i class="far fa-envelope"></i> &nbsp;Contact Me</a>
         </div>
         <div className="col-md-6 col-lg-6">
